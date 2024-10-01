7c8c25688de1ed23578914bacc461835d382ece8
RainRemm <rain.remmelgas@tktk.ee>
2024-10-01 16:28:21 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");//output
+//string - sõne
+string userName = Console.ReadLine();//input
+
+Console.WriteLine("Hello" + userName);//output
+
+Console.WriteLine($"hello, {userName} ?");//output
